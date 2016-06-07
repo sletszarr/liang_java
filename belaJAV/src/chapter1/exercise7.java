8d8de485d657845678f2dce0075f47d03c48b813
bnamora <bnamora@me.com>
2016-06-07 23:30:50 +0700

    [1,7] computing approximation of π

---
+++ belaJAV/src/chapter1/exercise7.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/7/16.
+ */
+public class exercise7 {
+
+    public static void main(String[] args) {
+
+        System.out.println("             1     1     1     1     1     1");
+        System.out.println("4 x (  1  -  -  +  -  -  -  +  -  -  -  +  -  ) = " + (4 * (1 - 1.0 / 3 + 1.0 / 5 - 1.0 / 7 + 1.0 / 9 - 1.0 / 11 + 1.0 / 13)));
+        System.out.println("             3     5     7     9     11    13");
+
+    }
+}
