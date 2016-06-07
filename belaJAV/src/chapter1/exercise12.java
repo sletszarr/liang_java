d33f435450e258b408a888c2027cd98cd80e5551
bnamora <bnamora@me.com>
2016-06-08 00:08:54 +0700

    [1,12] computing a runner's average speed in kilometers

---
+++ belaJAV/src/chapter1/exercise12.java
@@ -0,0 +1,17 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/8/16.
+ */
+public class exercise12 {
+
+    public static void main(String[] args) {
+
+        double kms = 24 * 1.6;
+        double hours = 1.0 + 40.0 / 60 + 35.0 / 3600;
+
+        System.out.println("A runner runs " + kms + " kms in " + hours + " hours");
+        System.out.println("His average speed is " + kms / hours + " km/hour");
+
+    }
+}
