3c151786eb03ed5c0ac39e4809eaeee50fd744e6
bnamora <bnamora@me.com>
2016-06-07 23:35:53 +0700

    [1,8] computing perimeter and area of a circle

---
+++ belaJAV/src/chapter1/exercise8.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/7/16.
+ */
+public class exercise8 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Radius: 5.5");
+        System.out.println("Perimeter: " + 2 * 5.5 * Math.PI);
+        System.out.println("Area : " + 5.5 * 5.5 * Math.PI);
+
+    }
+}
