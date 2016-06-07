541805aeaeec1311dbcf788f88c79f86550a9fdd
bnamora <bnamora@me.com>
2016-06-07 23:04:02 +0700

    [1,4] displaying [1-4] to the power of 2 and 3

---
+++ belaJAV/src/chapter1/exercise4.java
@@ -0,0 +1,14 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/7/16.
+ */
+public class exercise4 {
+    public static void main(String[] args) {
+        System.out.println("a       a^2     a^3");
+        System.out.println("1       "+(1*1)+"       "+(1*1*1));
+        System.out.println("2       "+(2*2)+"       "+(2*2*2));
+        System.out.println("3       "+(3*3)+"       "+(3*3*3));
+        System.out.println("4       "+(4*4)+"      "+(4*4*4));
+    }
+}
