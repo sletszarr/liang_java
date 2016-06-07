72f40b6449a67ed80494f6598caf1e6a2514ea33
bnamora <bnamora@me.com>
2016-06-07 22:41:06 +0700

    exercise 1, chapter 1

---
+++ belaJAV/src/chapter1/exercise1.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/7/16.
+ */
+public class exercise1 {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Computer Science");
+        System.out.println("Programming is Fun");
+    }
+}
