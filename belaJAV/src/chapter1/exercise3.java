0777e466d20a87e4df4003492682ff92c2f61aca
bnamora <bnamora@me.com>
2016-06-07 22:46:01 +0700

    [1,3] display 'JAVA' pattern

---
+++ belaJAV/src/chapter1/exercise3.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/7/16.
+ */
+public class exercise3 {
+    public static void main(String[] args) {
+        System.out.println("    J       A      V     V      A");
+        System.out.println("    J      A A      V   V      A A");
+        System.out.println("J   J     AAAAA      V V      AAAAA");
+        System.out.println(" J J     A     A      V      A     A ");
+    }
+}
