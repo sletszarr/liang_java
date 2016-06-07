a7c5ea1902b3c68f68042b875974533b2d5d397f
bnamora <bnamora@me.com>
2016-06-07 23:11:19 +0700

    [1,5] computing algebra expression

---
+++ belaJAV/src/chapter1/exercise5.java
@@ -0,0 +1,13 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/7/16.
+ */
+
+public class exercise5 {
+    public static void main(String[] args) {
+        System.out.println("9.5 x 4.5 - 2.5 x 3");
+        System.out.println("------------------- = " + (9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+        System.out.println("     45.5 - 3.5    ");
+    }
+}
