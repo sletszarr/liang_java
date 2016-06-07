9420c0c722e798aee10cc72a219848a770dcdfbc
bnamora <bnamora@me.com>
2016-06-07 23:49:42 +0700

    [1,10] computing a runner's average speed in mph

---
+++ belaJAV/src/chapter1/exercise10.java
@@ -0,0 +1,18 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/7/16.
+ */
+public class exercise10 {
+
+    public static void main(String[] args) {
+
+        double miles = 14 / 1.6;
+        double hours = 45.0 / 60 + 30.0 / 3600;
+
+        System.out.print("A runner run " + miles + " miles ");
+        System.out.println("in " + hours + " hour");
+        System.out.println("His average speed is " + miles / hours + " mph");
+
+    }
+}
