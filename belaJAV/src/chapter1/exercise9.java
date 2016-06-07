f95a36b0f8e7062968f325472da68c20acf25b69
bnamora <bnamora@me.com>
2016-06-07 23:42:54 +0700

    [1,9] computing area and perimeter of a rectangle

---
+++ belaJAV/src/chapter1/exercise9.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+/**
+ * Created by bnamora on 6/7/16.
+ */
+public class exercise9 {
+
+    public static void main(String[] args) {
+
+        System.out.println("width: 4.5");
+        System.out.println("height: 7.9");
+        System.out.println("area: " + 4.5 * 7.9);
+
+    }
+}
