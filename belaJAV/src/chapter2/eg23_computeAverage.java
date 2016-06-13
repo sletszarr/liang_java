999389be6b69e7238b07697b3c42c66a6b7c58f0
bnamora <bnamora@me.com>
2016-06-13 13:55:34 +0700

    [eg2,3] compute average of 3 numbers from input scanner

---
+++ belaJAV/src/chapter2/eg23_computeAverage.java
@@ -0,0 +1,24 @@
+package chapter2;
+
+import java.util.Scanner;
+
+/**
+ * Created by bnamora on 6/13/16.
+ */
+public class eg23_computeAverage {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("enter 3 numbers: ");
+        double firstNum = input.nextDouble();
+        double secondNum = input.nextDouble();
+        double thirdNum = input.nextDouble();
+
+        double average = (firstNum + secondNum + thirdNum) / 3;
+
+        System.out.println("the average of " + firstNum + ", " + secondNum + ", and " + thirdNum + " is " + average);
+
+    }
+}
