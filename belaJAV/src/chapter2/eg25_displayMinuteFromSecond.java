70e33c489ca2367aa5ff90f00e6c6605f695e020
bnamora <bnamora@me.com>
2016-06-13 14:39:28 +0700

    [eg2,5] display minutes from seconds

---
+++ belaJAV/src/chapter2/eg25_displayMinuteFromSecond.java
@@ -0,0 +1,23 @@
+package chapter2;
+
+import java.util.Scanner;
+
+/**
+ * Created by bnamora on 6/13/16.
+ */
+public class eg25_displayMinuteFromSecond {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("input a number of seconds: ");
+        int totalSeconds = input.nextInt();
+
+        int totalMinutes = totalSeconds / 60;
+        int remainingSeconds = totalSeconds % 60;
+
+        System.out.println(totalSeconds + " seconds is " + totalMinutes + " minutes and " + remainingSeconds + " seconds");
+
+    }
+}
