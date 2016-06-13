d3bb637a4af684c9ced13f2e317cf7458ead95fd
bnamora <bnamora@me.com>
2016-06-13 13:40:04 +0700

    [eg2,1] compute a circle's area with a fix radius

---
+++ belaJAV/src/chapter2/eg21_computeArea.java
@@ -0,0 +1,20 @@
+package chapter2;
+
+/**
+ * Created by bnamora on 6/13/16.
+ */
+public class eg21_computeArea {
+
+    public static void main(String[] args) {
+
+        double radius;
+        double area;
+
+        radius = 50.0;
+
+        area = radius * radius * Math.PI;
+
+        System.out.println("A circle's area with radius of " + radius + " is " + area);
+
+    }
+}
