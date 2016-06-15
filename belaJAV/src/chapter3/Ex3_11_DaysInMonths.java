19cc7e12ac6eb1bca809582ba50d2fed03cecd7e
bnamora <bnamora@me.com>
2016-06-15 21:27:11 +0700

    [Ex3,11] Calculating number of days in a given month and year

---
+++ belaJAV/src/chapter3/Ex3_11_DaysInMonths.java
@@ -0,0 +1,55 @@
+package chapter3;
+
+import java.util.Scanner;
+
+/**
+ * Created by bnamora on 6/15/16.
+ */
+
+public class Ex3_11_DaysInMonths {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter month: ");
+        int month = input.nextInt() - 1;
+
+        System.out.print("Enter year: ");
+        int year = input.nextInt();
+
+        int days = 0;
+        String[] monthName = {"January", "February", "March", "April", "Mei", "June", "July", "August", "September", "October", "November", "December"};
+
+        switch (month) {
+            case 0:
+            case 2:
+            case 4:
+            case 6:
+            case 7:
+            case 9:
+            case 11:
+                days = 31;
+                break;
+            case 3:
+            case 5:
+            case 8:
+            case 10:
+                days = 30;
+                break;
+            case 1:
+                if ((year % 4 == 0 || year % 400 == 0) && year % 100 != 0) {
+                    days = 29;
+                } else {
+                    days = 28;
+                }
+                break;
+            default:
+                System.out.println("You've entered invalid month");
+                System.exit(1);
+        }
+
+        System.out.println(monthName[month]+ " " + year + " had " + days + " days");
+
+    }
+}
