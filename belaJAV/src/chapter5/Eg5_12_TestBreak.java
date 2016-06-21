b1ba4c5ccbe86771408a10e5f5786b21ff3911e1
bnamora <bnamora@me.com>
2016-06-21 22:05:08 +0700

    [Eg5,12] Testing break syntax

---
+++ belaJAV/src/chapter5/Eg5_12_TestBreak.java
@@ -0,0 +1,27 @@
+package chapter5;
+
+/**
+ * Created by bnamora on 6/21/16.
+ */
+
+public class Eg5_12_TestBreak {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+        int number = 0;
+
+        while (number < 20) {
+            number++;
+            sum += number;
+
+            if (sum > 100)
+                break;
+
+        }
+
+        System.out.println("The number is " + number);
+        System.out.println("The sum is " + sum);
+
+    }
+}
