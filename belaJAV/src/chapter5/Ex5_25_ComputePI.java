c9d4f5be537a70ecb720235fe8b6fa5a95bb89fc
bnamora <bnamora@me.com>
2016-06-27 17:38:50 +0700

    [Ex5,25] Compute π value in 10,000 - 100,000 iterations

---
+++ belaJAV/src/chapter5/Ex5_25_ComputePI.java
@@ -0,0 +1,32 @@
+package chapter5;
+
+/**
+ * Created by bnamora on 6/27/16.
+ */
+
+public class Ex5_25_ComputePI {
+
+    public static void main(String[] args) {
+
+        System.out.println("iterations\t\t π value");
+
+        // iteration's step
+        for (int k = 10000; k <= 100000; k += 10000) {
+            double sumOfPISeries = 0.0;
+
+            // sum of pi series in k iterations
+            for (int l = k; l > 0; l--) {
+                double dividend = Math.pow(-1, (l % 2) + 1);
+                double divisor = 2.0 * l - 1;
+                sumOfPISeries += dividend / divisor;
+            }
+
+            // π value
+            double PI = 4 * sumOfPISeries;
+
+            System.out.printf("%6d\t\t\t%.10f", k, PI);
+            System.out.println();
+        }
+
+    }
+}
