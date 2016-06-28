01099e7ca66efedf57310cd272f2f30a87b5b069
bnamora <bnamora@me.com>
2016-06-28 22:51:38 +0700

    [Ex5,25] Compute π. Improvement: simplify the algorithm for determining positive/negative dividend in the summation series

--- belaJAV/src/chapter5/Ex5_25_ComputePI.java
+++ belaJAV/src/chapter5/Ex5_25_ComputePI.java
@@ -11,8 +11,6 @@ public class Ex5_25_ComputePI {
         System.out.println("iterations\t\t π value");
 
         double sumOfPISeries;
-        double dividend;
-        double divisor;
 
         // iteration's step
         for (int k = 10000; k <= 100000; k += 10000) {
@@ -20,9 +18,9 @@ public class Ex5_25_ComputePI {
 
             // sum of pi series in k iterations
             for (int l = k; l > 0; l--) {
-                dividend = Math.pow(-1, (l % 2) + 1);
-                divisor = 2.0 * l - 1;
-                sumOfPISeries += dividend / divisor;
+                sumOfPISeries -= 1.0 / (2.0 * l - 1);
+                l--;
+                sumOfPISeries += 1.0 / (2.0 * l - 1);
             }
 
             // π value
