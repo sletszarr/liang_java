d2de20d5cf324de1c5bc21e609607b70e82d5642
bnamora <bnamora@me.com>
2016-06-28 22:37:40 +0700

    [Ex5,26] Compute E. improvement: use double to hold factorial

--- belaJAV/src/chapter5/Ex5_26_ComputeE.java
+++ belaJAV/src/chapter5/Ex5_26_ComputeE.java
@@ -1,5 +1,7 @@
 package chapter5;
 
+import java.math.BigInteger;
+
 /**
  * Created by bnamora on 6/27/16.
  */
@@ -8,14 +10,22 @@ public class Ex5_26_ComputeE {
 
     public static void main(String[] args) {
 
-        double e = 1.0;
-        int factorial = 1;
+        double e;
+        double factorial;
 
-        for (int i = 1; i < 10; i++) {
-            factorial *= i;
-            e += 1.0 / factorial;
+        System.out.println("Iterations\t\tEuler Approximate");
+        for (int k = 10000; k <= 100000; k += 10000) {
+
+            e = 1.0;
+            factorial = 1.0;
+
+            for (int l = 1; l <= k; l++) {
+                factorial *= l;
+                e += 1.0 / factorial;
+            }
+
+            System.out.printf("\t%6d\t\t", k);
+            System.out.println(e);
         }
-
-        System.out.println(e);
     }
 }
