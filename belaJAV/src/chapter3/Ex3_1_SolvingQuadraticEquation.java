5cf263c9cb525ca5a30d8ec9f0a335e4172f06f4
CF <CF>
2016-07-07 23:54:59 +0700

    [Ex3,1] Solving quadratic eqn: add parenthesis to (2*a)

--- belaJAV/src/chapter3/Ex3_1_SolvingQuadraticEquation.java
+++ belaJAV/src/chapter3/Ex3_1_SolvingQuadraticEquation.java
@@ -22,14 +22,14 @@ public class Ex3_1_SolvingQuadraticEquation {
         double discriminant = Math.pow(b, 2) - 4 * a * c;
 
         if (discriminant > 0) {
-            double r1 = (-b + Math.pow(discriminant, 0.5)) / 2 * a;
-            double r2 = (-b - Math.pow(discriminant, 0.5)) / 2 * a;
+            double r1 = (-b + Math.pow(discriminant, 0.5)) / (2 * a);
+            double r2 = (-b - Math.pow(discriminant, 0.5)) / (2 * a);
 
             System.out.println("The equation has two roots " + r1 + " and " + r2);
 
         }
         else if (discriminant == 0) {
-            double r = -b / 2 * a;
+            double r = -b / (2 * a);
 
             System.out.println("The equation has one root " + r);
 
