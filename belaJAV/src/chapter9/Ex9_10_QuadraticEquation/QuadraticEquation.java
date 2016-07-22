2a7abcaf9c4d95aaaef13b9d133df7201c07e2d4
bnamora <bnamora@me.com>
2016-07-22 14:41:26 +0700

    [Ex9,10] Quadratic Equation

---
+++ belaJAV/src/chapter9/Ex9_10_QuadraticEquation/QuadraticEquation.java
@@ -0,0 +1,51 @@
+package chapter9.Ex9_10_QuadraticEquation;
+
+/**
+ * Created by bnamora on 7/22/16.
+ */
+
+public class QuadraticEquation {
+
+    private double a;
+    private double b;
+    private double c;
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public double getDiscriminant() {
+        return b * b - 4 * a * c;
+    }
+
+    public double getRoot1() {
+        if (this.getDiscriminant() < 0)
+            return 0;
+        else
+            return (-b + Math.sqrt(this.getDiscriminant()))
+                    / (2 * a);
+    }
+
+    public double getRoot2() {
+        if (this.getDiscriminant() < 0)
+            return 0;
+        else
+            return (-b - Math.sqrt(this.getDiscriminant()))
+                    / (2 * a);
+    }
+
+}
