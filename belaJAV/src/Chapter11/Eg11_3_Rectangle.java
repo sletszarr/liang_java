60cd69a279ea85219dcc1cbbf2626493e533413c
bnamora <bnamora@me.com>
2016-07-31 17:58:18 +0700

    [Eg11,3] Rectangle extends GeometricObject

---
+++ belaJAV/src/Chapter11/Eg11_3_Rectangle.java
@@ -0,0 +1,54 @@
+package Chapter11;
+
+/**
+ * Created by bnamora on 7/31/16.
+ */
+
+public class Eg11_3_Rectangle
+        extends Eg11_1_GeometricObject {
+
+    private double width;
+    private double height;
+
+    public Eg11_3_Rectangle() {
+    }
+
+    public Eg11_3_Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Eg11_3_Rectangle(double width, double height,
+                            String color, boolean filled) {
+        this.width = width;
+        this.height = height;
+        setColor(color);
+        setFilled(filled);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getArea() {
+        return width * height;
+    }
+
+    public double getPerimeter() {
+        return 2 * (width + height);
+    }
+}
+
+
