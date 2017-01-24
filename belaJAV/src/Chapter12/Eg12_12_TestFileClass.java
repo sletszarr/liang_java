b87844d4bc11f97e69b9712b6199fd32f940c389
sletszarr <bnamora@aurasoft.co>
2017-01-24 11:09:51 +0700

    [Eg12,12] Testing File Class

---
+++ belaJAV/src/Chapter12/Eg12_12_TestFileClass.java
@@ -0,0 +1,29 @@
+package Chapter12;
+
+import java.io.File;
+import java.util.Date;
+
+/**
+ * Created by bnamora on 1/24/17.
+ */
+
+public class Eg12_12_TestFileClass {
+
+    public static void main(String[] args) {
+
+        File file = new File(".gitignore");
+
+        System.out.println("Does it exist? " + file.exists());
+        System.out.println("The file has " + file.length() + " bytes");
+        System.out.println("Can it be read? " + file.canRead());
+        System.out.println("Can it be written? " + file.canWrite());
+        System.out.println("Is it a directory? " + file.isDirectory());
+        System.out.println("Is it a file? " + file.isFile());
+        System.out.println("Is it absolute? " + file.isAbsolute());
+        System.out.println("Is it hidden? " + file.isHidden());
+        System.out.println("Absolute path is " + file.getAbsolutePath());
+        System.out.println("Last modified on " + new Date(file.lastModified()));
+
+    }
+
+}
